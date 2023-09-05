df82e00646bbfa89478a8d34469d3e5310387378
Ernane Ferreira <ernane.junior25@gmail.com>
2023-09-05 20:10:16 -0300

    wip

--- autocomplete_words/main.py
+++ autocomplete_words/main.py
@@ -13,28 +13,27 @@ def build_structures(autocomplete):
   return array_list, avl_tree
 
 def init():
-  autocomplete = AutocompleteWords('autocomplete_words/cinco_minutos.txt')
+  autocomplete = AutocompleteWords('autocomplete_words/assets/cinco_minutos.txt')
   array_list, avl_tree = build_structures(autocomplete)
 
   prefix = "a"
   
-  print(".:: Searching the AVL tree")
   avl_tree_results = autocomplete.search(avl_tree.root, prefix)
-    
-  print(f"    => prefix: {prefix}")
-  print(f"    => count_words: {avl_tree_results['count_words']}")
-  print(f"    => file_path: {avl_tree_results['file_path']}")
-  print(f"    => file_size: {avl_tree_results['file_size']}")
-  print(f"    => execution_time: {avl_tree_results['execution_time']}")
+  print(".:: Searching the AVL tree")
+  print(f"    => prefix: '{prefix}'")
+  print(f"    => count_words: {avl_tree_results['count_words']} words found")
+  print(f"    => file_path: '{avl_tree_results['file_path']}'")
+  print(f"    => file_size: {avl_tree_results['file_size'] / (1024 * 1024)} MB")
+  print(f"    => execution_time: {avl_tree_results['execution_time']} seconds")
   print(".:: Search ended.\n")
   
   array_list_results = autocomplete.search(array_list, prefix)
   print(".:: Searching the Array List")
-  print(f"    => prefix: {prefix}")
-  print(f"    => count_words: {avl_tree_results['count_words']}")
-  print(f"    => file_path: {avl_tree_results['file_path']}")
-  print(f"    => file_size: {avl_tree_results['file_size']}")
-  print(f"    => execution_time: {avl_tree_results['execution_time']}")
+  print(f"    => prefix: '{prefix}'")
+  print(f"    => count_words: {avl_tree_results['count_words']} words found")
+  print(f"    => file_path: '{avl_tree_results['file_path']}'")
+  print(f"    => file_size: {avl_tree_results['file_size'] / (1024 * 1024)} MB")
+  print(f"    => execution_time: {avl_tree_results['execution_time']} seconds")
   print(".:: Search ended.")
   
 init()
