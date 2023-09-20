64d98364a84b555480913eafa31c3c9fd56d15de
Ernane Ferreira <ernane.junior25@gmail.com>
2023-09-20 16:14:10 -0300

    wip: add find_laptops_with_features function

--- project_building_fast_queries_on_a_CSV/main.py
+++ project_building_fast_queries_on_a_CSV/main.py
@@ -1,25 +1,34 @@
 import csv
+import re
+
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 def row_price(row):
     return row[-1]
 
 class Inventory():                    
-    
     def __init__(self, csv_filename):
-        with open(csv_filename) as f: 
-            reader = csv.reader(f)
-            rows = list(reader)
-        self.header = rows[0]        
-        self.rows = rows[1:]
-        for row in self.rows:              
-            row[-1] = int(row[-1])
-        self.id_to_row = {}                        
-        for row in self.rows:                       
-            self.id_to_row[row[0]] = row
-        self.prices = set()                          
-        for row in self.rows:                        
-            self.prices.add(row[-1])
-        self.rows_by_price = sorted(self.rows, key=row_price) # Step 1
+      with open(csv_filename) as f: 
+        reader = csv.reader(f)
+        rows = list(reader)
+      self.header = rows[0]        
+      self.rows = rows[1:]
+      for row in self.rows:              
+        row[-1] = int(row[-1])
+      self.id_to_row = {}                        
+      for row in self.rows:                       
+        self.id_to_row[row[0]] = row
+      self.prices = set()                          
+      for row in self.rows:                        
+        self.prices.add(row[-1])
+      self.rows_by_price = sorted(self.rows, key=row_price)
+
+      self.all_features = {}
+      for key in self.header:
+        self.all_features[key] = [str(row[self.header.index(key)]).lower() for row in self.rows_by_price]
     
     def get_laptop_from_id(self, laptop_id):
         for row in self.rows:                 
@@ -112,6 +121,71 @@ class Inventory():
               range_end = range_middle - 1
       
       return laptops_in_range
+    
+    def find_laptops_with_features(self, features):
+      """
+      Finds laptops with specific features.
+
+      Args:
+          features (dict): A dictionary specifying the desired laptop features.
+                          Supported keys: ['Id', 'Company', 'Product', 'TypeName', 'Inches',
+                          'ScreenResolution', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys',
+                          'Weight', 'Price']. Any other keys are ignored.
+
+      Returns:
+          list: A list of laptops that match the specified features. Each laptop is represented
+                as a list of attributes, where the last element of the list is the price.
+      """
+      matching_laptops = []  # Initialize a list to store laptops that match the features
+      range_start = 0        # Initialize the start of the search range
+      range_end = len(self.rows_by_price) - 1  # Initialize the end of the search range
+      
+      while range_start <= range_end:
+          range_middle = (range_end + range_start) // 2  # Calculate the midpoint of the range
+          laptop = self.rows_by_price[range_middle]     # Get the laptop at the midpoint
+          price = laptop[-1]                            # Get the price of the laptop
+          
+          # Check if the laptop matches the specified features efficiently
+          match = all(key in self.header and laptop[self.header.index(key)] == value for key, value in features.items())
+          
+          # If the laptop matches the features, add it to the list
+          if match:
+              matching_laptops.append(laptop)
+          
+          # Update the search range boundaries based on the laptop's price
+          if price < laptop[-1]:
+              range_start = range_middle + 1
+          else:
+              range_end = range_middle - 1
+      
+      return matching_laptops  # Return the list of laptops that match the specified features
+
+    def get_desired_features_by_str(self, sentence):
+      stop_words = set(stopwords.words('portuguese'))
+
+      sentence = sentence.lower()
+      words.replace(" ssd", "-ssd")
+
+      words = sentence.split(' ')
+      filtered_words = [word for word in words if word not in stop_words]
+
+      desired_features = {}
+
+      print(self.rows_by_price[0])
+      print(filtered_words)
+
+      for feature in self.header:
+        for word in filtered_words:
+            if(word in self.all_features[feature]):
+                desired_features[feature] = word
+
+      print(desired_features)
+      
 
 inventory = Inventory('project_building_fast_queries_on_a_CSV/laptops.csv')
-print(inventory.header)
+
+desired_features = {
+  'Company': 'Acer',
+}
+
+print(inventory.get_desired_features_by_str('computador acer com 256gb de ram e 1TB HD'))
